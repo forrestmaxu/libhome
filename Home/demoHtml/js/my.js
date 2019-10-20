d983b8609dea848748899584f6a3a827a205708e
forrestmaxu <maxu0512@163.com>
2019-10-20 23:13:22 +0800

    weblib-commit

---
+++ Home/demoHtml/js/my.js
@@ -0,0 +1,15 @@
+$(function(){
+
+        $("#pro_lsit").click(function(){
+                $(".dropdown-menu").show()
+
+        })
+
+
+
+//         $('.tab-content:last').tab('show');//初始化显示哪个tab
+
+
+
+
+});
